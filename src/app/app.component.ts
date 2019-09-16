81d2fa20fe083d129abc0d7b90a264ed6a8c0564
Jaime R. Gonzalez M <arcarius11@gmail.com>
2019-09-16 17:42:20 -0300

    initial commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,13 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'task-three';
+  displayParagraph = false
+  buttonClicks = []
+
+  buttonParagraph() {
+    this.displayParagraph = !this.displayParagraph
+    // this.buttonClicks.push(this.buttonClicks.length + 1)
+    this.buttonClicks.push(new Date())
+    // console.log(this.buttonClicks)
+  }
 }
